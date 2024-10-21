97553cddee30cff24ed0e952c7c92fcc2af00dbb
권찬호 <rnjscksdud17@gmail.com>
2024-10-21 14:48:46 +0900

    초기 커밋.

---
+++ G24W08Lotto/Form1.cs
@@ -0,0 +1,39 @@
+namespace G24W08Lotto
+{
+    public partial class Form1 : Form
+    {
+        private const int LottoCount = 6;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenerate(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+            int[] num = new int[LottoCount];
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int n = 0;
+                do
+                {
+                    n = r.Next(1, 46);
+                } while (num.Contains(n));
+                //} while (Array.IndexOf(num, n) > -1);
+
+                num[i] = n;
+            }
+
+            Array.Sort(num);
+
+            Num1.Text = num[0].ToString();
+            Num2.Text = num[1].ToString();
+            Num3.Text = num[2].ToString();
+            Num4.Text = num[3].ToString();
+            Num5.Text = num[4].ToString();
+            Num6.Text = num[5].ToString();
+        }
+    }
+}
